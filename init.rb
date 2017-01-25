9338e96f49de937a6a61665067e0df15744a020b
Dmitry A <mr.ads@yandex.ru>
2017-01-25 09:16:04 +0300

    first

---
+++ init.rb
@@ -0,0 +1,8 @@
+Redmine::Plugin.register :redmine_telegram_bot do
+  name 'Redmine Telegram Bot plugin'
+  author 'Dmitry Anikin'
+  description 'This is a plugin for Redmine'
+  version '0.0.1'
+  url 'https://github.com/d-anikin/redmine_telegram_bot'
+  author_url 'https://github.com/d-anikin'
+end
